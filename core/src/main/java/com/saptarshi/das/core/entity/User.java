bf738ce8cdc4adb9ccd40666eeccf941163a5c1a
Saptarshi Das <saptarshidas743@gmail.com>
2024-01-27 19:27:31 +0530

    Remove warning related to equals and hashcode generators by lombok in entities

--- core/src/main/java/com/saptarshi/das/core/entity/User.java
+++ core/src/main/java/com/saptarshi/das/core/entity/User.java
@@ -4,12 +4,14 @@ import com.saptarshi.das.core.entity.embeddable.Name;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.experimental.Accessors;
 
 @Data
 @Entity
 @Accessors(chain = true)
 @Table(name = "APP_USER")
+@EqualsAndHashCode(callSuper = true)
 public class User extends BaseEntity {
     private Name name;
     private String email;
