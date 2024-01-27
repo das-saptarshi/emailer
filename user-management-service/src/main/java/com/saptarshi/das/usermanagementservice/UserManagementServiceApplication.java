556953038b9eb1003fe79032938d716ae9302395
Saptarshi Das <saptarshidas743@gmail.com>
2024-01-27 19:56:54 +0530

    Add entity scan annotation for user management service

--- user-management-service/src/main/java/com/saptarshi/das/usermanagementservice/UserManagementServiceApplication.java
+++ user-management-service/src/main/java/com/saptarshi/das/usermanagementservice/UserManagementServiceApplication.java
@@ -2,7 +2,9 @@ package com.saptarshi.das.usermanagementservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
+@EntityScan(basePackages = "com.saptarshi.das.core.entity")
 @SpringBootApplication
 public class UserManagementServiceApplication {
 
